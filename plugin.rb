604b0cac6e58d54b9945131dd141eda9a15d0bb9
Alan Liang <alan-liang@keeer.net>
2021-11-06 22:49:18 +0800

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# name: pm-convert
+# about: enable programming TAs to convert PMs into topics
+# version: 0.1
+# authors: Alan Liang
+# url: https://github.com/acm-21/discourse-pm-convert.git
+
+PLUGIN_NAME ||= 'PmConvert'
+
+after_initialize do
+  class ::Guardian
+    module OverridingGuardian
+      def can_convert_topic?(topic)
+        return true if super(topic)
+        return true if @user.groups.any? {|group| group.name == 'Programming-TAs' }
+        false
+      end
+    end
+    prepend OverridingGuardian
+  end
+end
